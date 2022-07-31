a934628ed1172a0bee30c7dd44c43c1e3fddca03
Stephen Trickey <itstrickey@gmail.com>
2022-07-31 16:40:12 -0400

    chopped debug info added in along the way, starting to access images from the image sheet, clipping out the first image to display

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-//console.log(ctx);
-
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
-let x = 0;
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(x, 50, 100, 100);
-    x++;//
-    x = x % CANVAS_WIDTH;
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
     requestAnimationFrame(animate);
 };
 animate();
