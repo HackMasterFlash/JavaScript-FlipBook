6dc0741989e142829627e7cb68b91c0d81d2cc2f
Stephen Trickey <itstrickey@gmail.com>
2022-09-03 16:50:11 -0400

    added mechanism to throttle image display

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ const staggerFrame = 4;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight,
+        spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrame == 0){
+        if (frameX < 6) frameX++;
+        else frameX = 0;
+    }
 
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
